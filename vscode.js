a65149c84f1830c970e558429ba66152d570075f
Nate Look <nate@natelook.com>
2019-04-08 14:07:15 -0700

    Create vscode.js

---
+++ vscode.js
@@ -0,0 +1,19 @@
+{
+  "workbench.colorTheme": "Dracula",
+  "workbench.iconTheme": "vscode-icons",
+  "atomKeymap.promptV3Features": true,
+  "editor.multiCursorModifier": "ctrlCmd",
+  "editor.formatOnPaste": true,
+  "prettier.eslintIntegration": true,
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact"
+  },
+  "workbench.activityBar.visible": false,
+  "editor.formatOnSave": true,
+  "eslint.autoFixOnSave": true,
+  "eslint.alwaysShowStatus": true,
+  "editor.autoIndent": true,
+  "editor.minimap.enabled": false,
+  "editor.tabSize": 2,
+  "window.title": "${dirty} ${activeEditorMedium}${separator}${rootName}"
+}
